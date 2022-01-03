ef9ef57bb004fb17b6d66c66704bfd3bcf8a8c98
RODRIGO GALLEGO <50471819+ragallegom@users.noreply.github.com>
2022-01-03 17:01:14 -0500

    refactor: added file views fortify services

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -16,10 +16,6 @@ class FortifyServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Fortify::loginView(function (){
-            return view('auth.login');
-        });
-
         Fortify::registerView(function (){
             return view('auth.register');
         });
