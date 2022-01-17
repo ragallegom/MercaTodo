2a09424a7cd9780c5a077542554035e83995c483
RODRIGO GALLEGO <50471819+ragallegom@users.noreply.github.com>
2022-01-17 14:42:59 -0500

    feat: create controller for Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
+use App\Http\Requests\ProductIndexRequest;
+use App\Models\Category;
+use App\Cart;
+use Session;
 
 class ProductController extends Controller
 {
-    public function index(Request $request): View
+    public function index(ProductIndexRequest $request): View
     {
         $searchName = trim($request->get('searchName'));
         $searchDescription = trim($request->get('searchDescription'));
@@ -18,7 +23,6 @@ class ProductController extends Controller
         $categories = DB::table('categories')
             ->where('name','LIKE', '%' . $searchName . '%')
             ->where('description','LIKE', '%' . $searchDescription . '%')
-            ->where('')
             ->whereNull('disabled_at')
             ->orderBy('id', 'desc')
             ->paginate(7);
@@ -30,5 +34,41 @@ class ProductController extends Controller
             "searchRangeMin" => $searchRangeMin,
             "searchRangeMax" => $searchRangeMax
         ]);
+    }
+
+    public function getAddToCart(Request $request, string $id): RedirectResponse
+    {
+        $product = Category::findOrFail($id);
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->add($product, $product->id);
+
+        $request->session()->put('cart', $cart);
+        return redirect()->route('products.index');
+    }
+
+    public function getCart(): view
+    {
+        if (!Session::has('cart'))
+        {
+            return view('store.products.shopping-cart', ['products' => null]);
+        }
+
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+        return view('store.products.shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
+    }
+
+    public function getCheckout(): view
+    {
+        if(!Session::has('cart'))
+        {
+            return view('store.products.shopping-cart');
+        }
+
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+        $total = $cart->totalPrice;
+        return view('store.products.checkout', ['total' => $total]);
     }
 }
