bda68cfb9171c1cb5fc650ed0060216882db6e4f
RODRIGO GALLEGO <50471819+ragallegom@users.noreply.github.com>
2022-01-25 18:28:36 -0500

    feat: add new function to remove products

--- app/Cart.php
+++ app/Cart.php
@@ -18,7 +18,7 @@ class Cart
         }
     }
 
-    public function add($item, string $id)
+    public function add($item, string $id): void
     {
         $storedItem = ['qty' => 0, 'price' => $item->price, 'item' => $item];
         if ($this->items)
@@ -33,5 +33,31 @@ class Cart
         $this->items[$id] = $storedItem;
         $this->totalQty++;
         $this->totalPrice += $item->price;
+    }
+
+    public function remove($item, string $id): void
+    {
+        $storedItem = ['qty' => 0, 'price' => $item->price, 'item' => $item];
+        if ($this->items)
+        {
+            if (array_key_exists($id, $this->items))
+            {
+                $storedItem = $this->items[$id];
+            }
+        }
+        if($storedItem['qty'] > 1)
+        {
+            $storedItem['qty']--;
+            $storedItem['price'] = $item->price * $storedItem['qty'];
+            $this->items[$id] = $storedItem;
+            $this->totalQty--;
+            $this->totalPrice -= $item->price;
+        }
+        else
+        {
+            $this->totalQty--;
+            $this->totalPrice -= $item->price;
+            unset($this->items[$id]);
+        }
     }
 }
